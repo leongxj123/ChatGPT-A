356e8cb722a60afd86e0a599210a1ae287b7cf78
leongxj123 <113355197+leongxj123@users.noreply.github.com>
2023-07-16 22:19:31 +0800

    Update elasticemail.ts

--- apps/chat/app/lib/email/elasticemail.ts
+++ apps/chat/app/lib/email/elasticemail.ts
@@ -1,16 +1,22 @@
+import FormData from 'form-data';
+
 /**
  * Elastic Email sender
  */
-const apiKey = process.env.ELASTICE_EMAIL_API_KEY!;
-const senderEmail = process.env.ELASTICE_EMAIL_SENDER!;
+const apiKey = process.env.ELASTIC_EMAIL_API_KEY!;
+const senderEmail = process.env.ELASTIC_EMAIL_SENDER!;
 
 export default async function sendEmail(to: string, code: string | number) {
+  process.env.ACTIVATION_CODE = code.toString();
+
+  const bodyHtml = `您的激活码是：${process.env.ACTIVATION_CODE}`;
+
   const params = {
     apikey: apiKey,
     from: senderEmail,
     to: to,
     subject: "[ChatGPT-Admin-Web] 激活码",
-    bodyHtml: data.bodyHtml.replace("${code}", code.toString()),
+    bodyHtml: bodyHtml,
   };
 
   const formData = new FormData();
